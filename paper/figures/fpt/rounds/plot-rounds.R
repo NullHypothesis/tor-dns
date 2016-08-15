fc285edf110a80fd87f10543c2de370385564233
Philipp Winter <phw@nymity.ch>
2016-08-15 15:40:58 -0400

    Harmonise legend position.

--- paper/figures/fpt/rounds/plot-rounds.R
+++ paper/figures/fpt/rounds/plot-rounds.R
@@ -39,7 +39,7 @@ ggplot(df, aes(x, y, colour = Attack,
           legend.key.height = unit(0.8, "line"),
           legend.key = element_rect(colour = "transparent", fill = "transparent"),
           legend.background = element_rect(colour = "gray", fill = "white", size = 0.3),
-          legend.position = c(1, 0)) +
+          legend.position = c(1.05, -0.05)) +
     scale_color_brewer(palette = "Set1")
 
 dev.off()
