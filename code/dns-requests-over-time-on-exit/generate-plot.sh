d10804f3e28e1b80f737a366c644a55c76f1fdfe
Philipp Winter <phw@nymity.ch>
2016-09-02 10:42:01 -0400

    Delete file that's no longer used.

--- code/dns-requests-over-time-on-exit/generate-plot.sh
+++ code/dns-requests-over-time-on-exit/generate-plot.sh
@@ -6,4 +6,4 @@ Rscript plot-dns-requests.R 2016-05-15-to-2016-05-31-80-and-443.csv
 pdfcrop dns-requests.pdf \
         dns-requests.pdf
 
-cp dns-reqs.pdf "$FIGURES_DIR"
+cp dns-requests.pdf "$FIGURES_DIR"
